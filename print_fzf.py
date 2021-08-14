250494a31dd4acb8516b2fadf0a029a9d24d2795
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-08-14 14:13:14 +0200

    Fix some issues with coloring.

--- print_fzf.py
+++ print_fzf.py
@@ -3,6 +3,8 @@ import os,sys
 
 dn = os.path.dirname(os.path.realpath(__file__))
 fzf_file = open(dn + "/fzf.txt").read()
+fzf_file = fzf_file.replace("[0;34m", "")
+fzf_file = fzf_file.replace('[0m', '')
 
 print(fzf_file)
 sys.exit()
