81c661b776de196949e9c09033af8d62c5af91e8
CronyAkatsuki <crolinuxgamer@gamil.com>
2022-02-03 16:25:38 +0100

    Unicode support ;)

--- print_fzf.py
+++ print_fzf.py
@@ -2,9 +2,8 @@ import os,sys
 #just a quick script to print the fzf.txt file content
 
 dn = os.path.dirname(os.path.realpath(__file__))
-fzf_file = open(dn + "/fzf.txt").read()
-fzf_file = fzf_file.replace("[0;34m", "")
-fzf_file = fzf_file.replace('[0m', '')
-
+fzf_file = open(dn + "/fzf.txt", encoding="utf-8").read()
+fzf_file = fzf_file.replace("[0;34m", "[0;33m")
+fzf_file = fzf_file.replace('[0m', '[0m')
 print(fzf_file)
 sys.exit()
